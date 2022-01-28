0ad9954f116e9f1acd43ef0c1856f32b8cce8d4a
Ell <alan@cattes.us>
2022-01-28 02:08:19 -0600

    fix build

--- src/Ticker.tsx
+++ src/Ticker.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react';
 import useSound from 'use-sound';
 
+// @ts-ignore
 import fart1 from './farts/fart1.mp3?url';
+// @ts-ignore
 import fart2 from './farts/fart2.mp3?url';
+// @ts-ignore
 import fart3 from './farts/fart3.mp3?url';
+// @ts-ignore
 import fart4 from './farts/fart4.mp3?url';
+// @ts-ignore
 import fart5 from './farts/fart5.mp3?url';
+// @ts-ignore
 import fart6 from './farts/fart6.mp3?url';
+// @ts-ignore
 import fart7 from './farts/fart7.mp3?url';
 
 import { OHLCPayload } from './TickerContainer';
